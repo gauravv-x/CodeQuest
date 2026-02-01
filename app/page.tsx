307068634b31a5d3d3d8b7b90171f36de01623a3
gauravv-x <gauravakabari22@gnu.ac.in>
2026-02-01 18:57:43 +0530

    Home page Creation

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,29 @@
-import { Button } from "@base-ui/react";
 import Image from "next/image";
+import Header from "./_components/Header";
+import Hero from "./_components/Hero";
+import { Button } from "@/components/ui/button";
+
+
+/**
+ * Defual page of the application.
+ * @returns 
+ */
+
+
 
 export default function Home() {
   return (
-    <div>
-      <h1>Welcome to the Home Page</h1>
-      <Button> ok </Button>
+    <div className="flex flex-col items-center">
+      {/* Header / Navbar */}
+      <Header />
+      
+
+      {/* Hero Section */}
+      <Hero />
+
+      
+      
+  
     </div>
   );
 }
