f8f18983da83df24075afeda3b4dd9518fa52fbc
gauravv-x <gauravakabari22@gnu.ac.in>
2026-02-02 12:48:50 +0530

    feat: add user management and database integration
    - Added new dependencies for Neon database and Drizzle ORM.
    - Created API route for user management to handle user creation and retrieval.
    - Implemented UserDetailContext for managing user state in the application.
    - Configured database connection using Drizzle ORM with PostgreSQL.
    - Defined user schema for the database with necessary fields.

---
+++ app/api/user/route.ts
@@ -0,0 +1,32 @@
+import { db } from "@/config/db";
+import { usersTable } from "@/config/schema";
+import { currentUser } from "@clerk/nextjs/server";
+import { eq, exists } from "drizzle-orm";
+import { NextRequest, NextResponse } from "next/server";
+
+export async function POST(req: NextRequest) {
+
+    const user= await currentUser();
+
+    //@ts-ignore if user  already exists 
+    const users = await db.select().from(usersTable).where(eq(usersTable.email, user?.primaryEmailAddress?.emailAddress));
+
+    // if not create new user data 
+    if (users.length <= 0) {
+        const newUser = {
+            name: user?.fullName??'',
+            email: user?.primaryEmailAddress?.emailAddress??'',
+            points: 0,
+            
+        }
+
+        const result = await db.insert(usersTable)
+        .values(newUser).returning();
+
+        return NextResponse.json(result[0]);    
+
+    }
+
+    return NextResponse.json({user:users[0]});
+
+}
